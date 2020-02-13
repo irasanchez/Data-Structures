47eb7ea56ebbbbfc2fa4601caedbfd9c90390f21
Ira <ira.d.sanchez@gmail.com>
2020-02-12 20:17:04 -0700

    Add Pop Method To Stack

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -16,7 +16,15 @@ class Stack:
         self.storage.add_to_head(value)
 
     def pop(self):
-        pass
+        # Remove an item from the end
+        # Return new list with the last item removed from it
+        # Check if the list is empty, because of it's empty you can take something off of it
+        if self.size > 0:
+            self.size -= 1
+            # Use remove from head method from the doubly linked list
+            return self.storage.remove_from_head()
+        else:
+            return None  # Nothing is there
 
     def len(self):
         pass
