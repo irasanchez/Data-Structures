5ada9ed0307746b7f9079738a2c43b12b762f591
Ira <ira.d.sanchez@gmail.com>
2020-02-14 19:25:29 -0700

    Add completed move_to_front

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -97,7 +97,9 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        if node is not self.head:
+            self.delete(node)
+            self.add_to_head(node.value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
