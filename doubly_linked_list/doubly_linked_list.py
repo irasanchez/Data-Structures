556f84a1bb7743e52039f97b97c38afb6125c29a
Ira <ira.d.sanchez@gmail.com>
2020-02-20 20:28:46 -0700

    Add move_to_end to DLL

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -127,7 +127,12 @@ class DoublyLinkedList:
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
-        pass
+        if node is not self.tail:
+            # 1. remove
+            self.delete(node)
+
+            # 2. insert @ tail
+            self.add_to_tail(node.value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
